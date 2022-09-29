736580f37a911b7ad5766c26b877b08658c79dbd
Buyole Isacko <isackobuyole@gmail.com>
2022-09-29 17:28:38 +0300

    called view model in the main activity

--- app/src/main/java/com/example/mvvmappwithkotlin/View/MainActivity.kt
+++ app/src/main/java/com/example/mvvmappwithkotlin/View/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mvvmappwithkotlin
+package com.example.mvvmappwithkotlin.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmappwithkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
