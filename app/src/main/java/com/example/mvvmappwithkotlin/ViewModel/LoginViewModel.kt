c3c1868b1a76c9131b35d08c2f111b1db3198e0e
Buyole Isacko <isackobuyole@gmail.com>
2022-09-29 16:25:23 +0300

    implemented log in view model

--- app/src/main/java/com/example/mvvmappwithkotlin/ViewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/mvvmappwithkotlin/ViewModel/LoginViewModel.kt
@@ -2,6 +2,7 @@ package com.example.mvvmappwithkotlin.ViewModel
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import androidx.lifecycle.ViewModel
 import com.example.mvvmappwithkotlin.Interface.LoginResultCallBacks
 import com.example.mvvmappwithkotlin.Model.User
@@ -9,11 +10,15 @@ import com.example.mvvmappwithkotlin.Model.User
 class LoginViewModel(private val listener: LoginResultCallBacks) : ViewModel() {
     private val user: User
 
+    init {
+        this.user = User("", "")
+    }
+
     //Create function to set email after user finish enter text
     val emailTextWatcher: TextWatcher
         get() = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                user.setEmail()
+                user.setEmail(s.toString())
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
@@ -25,4 +30,31 @@ class LoginViewModel(private val listener: LoginResultCallBacks) : ViewModel() {
             }
 
         }
+
+    //create function to set password after user finish enter text
+
+    val passwordTextWatcher: TextWatcher
+        get() = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                user.setPassword(s.toString())
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+    //create function to process login button when clicked
+
+    fun onLoginClicked(v:View){
+        if (user.isDataValid)
+            listener.onSuccess("Login Success")
+        else
+            listener.onError("Login failed")
+    }
 }
---
+++ app/src/main/java/com/example/mvvmappwithkotlin/ViewModel/LoginViewModelFactory.kt
@@ -0,0 +1,4 @@
+package com.example.mvvmappwithkotlin.ViewModel
+
+class LoginViewModelFactory {
+}
