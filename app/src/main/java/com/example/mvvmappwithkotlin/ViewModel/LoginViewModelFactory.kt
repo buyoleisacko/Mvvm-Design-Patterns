d10fc592605fefc61252c35cab3024da78a13892
Buyole Isacko <isackobuyole@gmail.com>
2022-09-29 16:33:37 +0300

    implemented log in view model factory

--- app/src/main/java/com/example/mvvmappwithkotlin/ViewModel/LoginViewModelFactory.kt
+++ app/src/main/java/com/example/mvvmappwithkotlin/ViewModel/LoginViewModelFactory.kt
@@ -1,4 +1,12 @@
 package com.example.mvvmappwithkotlin.ViewModel
 
-class LoginViewModelFactory {
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import com.example.mvvmappwithkotlin.Interface.LoginResultCallBacks
+
+class LoginViewModelFactory(private val listener:LoginResultCallBacks):
+    ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return LoginViewModel(listener) as T
+    }
 }
